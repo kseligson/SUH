49ee5c341fee512e8972e276be745cf9f773bf12
Kaitlin Seligson <kseligso@ucsd.edu>
2016-05-13 14:42:32 -0700

    Initial coding

---
+++ server.js
@@ -0,0 +1,71 @@
+//dependencies for each module used
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var dotenv = require('dotenv');
+var pg = require('pg');
+var app = express();
+
+//client id and client secret here, taken from .env (which you need to create)
+dotenv.load();
+
+var router = {
+  index: require("./routes/index"),
+  location: require("./routes/location"),
+  idea: require("./routes/idea")
+};
+
+//connect to database
+var conString = process.env.DATABASE_CONNECTION_URL;
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    console.error('could not connet', err);
+  }
+  else {
+    console.log("Successfully connected");
+  }
+});
+
+//Configures the Template engine
+app.engine('html', handlebars());
+app.set("view engine", "html");
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'keyboard cat',
+                  saveUninitialized: true,
+                  resave: true}));
+
+//set environment ports and start application
+app.set('port', process.env.PORT || 3000);
+
+//routes
+app.get('/', router.index.view);
+app.get('/findout', router.idea.view);
+app.get('/getaclue', router.location.view);
+
+app.get('/delphidata', function (req, res) {
+  // TODO
+  // Connect to the DELPHI Database and return the proper information
+  // that will be displayed on the D3 visualization
+  // Table: Smoking Prevalance in Adults
+  // Task: In the year 2003, retrieve the total number of respondents
+  // for each gender. 
+  // Display that data using D3 with gender on the x-axis and 
+  // total respondents on the y-axis.
+  client.query("SELECT gender, number_of_respondents FROM cogs121_16_raw.cdph_smoking_prevalence_in_adults_1984_2013 WHERE year = 2003 ",function(err,data){
+      res.json(data.rows);
+  });
+  //return { delphidata: "No data present." }
+});
+
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
