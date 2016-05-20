de66eef7f2426324251f6d8abefdcb5f4daee113
Kirk Srijongsirikul <ksrijong@ucsd.edu>
2016-05-19 18:24:46 -0700

    Hovering over region changes its color

--- public/js/overlay.js
+++ public/js/overlay.js
@@ -10,49 +10,13 @@ function initMap() {
   });
 
   map.data.loadGeoJson('./json/county2.json');
-  // map.data.setStyle({
-  //   fillColor: 'gray',
-  //   strokeWeight: 1
-  // });
+  map.data.setStyle({
+    fillColor: 'gray',
+    strokeWeight: 1
+  });
 
   map.data.addListener('mouseover', function(event) {
-    // document.getElementById('info-box').textContent =
-    //     event.feature.getProperty('letter');
-    map.data.overrideStyle(event.feature.setProperty({
-      fillColor: 'red'
-    }));
-  });
-
-  // Color each letter gray. Change the color when the isColorful property
-  // is set to true.
-  map.data.setStyle(function(feature) {
-    var color = 'gray';
-    if (feature.getProperty('isColorful')) {
-      color = feature.getProperty('color');
-    }
-    return /** @type {google.maps.Data.StyleOptions} */({
-      fillColor: color,
-      strokeColor: color,
-      strokeWeight: 2
-    });
-  });
-
-  // Reference: https://developers.google.com/maps/documentation/javascript/datalayer#change_appearance_dynamically
-
-  // When the user clicks, set 'isColorful', changing the color of the letters.
-  map.data.addListener('click', function(event) {
-    event.feature.setProperty('isColorful', true);
-  });
-
-  // When the user hovers, tempt them to click by outlining the letters.
-  // Call revertStyle() to remove all overrides. This will use the style rules
-  // defined in the function passed to setStyle()
-  map.data.addListener('mouseover', function(event) {
     map.data.revertStyle();
-    map.data.overrideStyle(event.feature, {strokeWeight: 8});
-  });
-
-  map.data.addListener('mouseout', function(event) {
-    map.data.revertStyle();
+    map.data.overrideStyle(event.feature, {fillColor: 'black'});
   });
 }
